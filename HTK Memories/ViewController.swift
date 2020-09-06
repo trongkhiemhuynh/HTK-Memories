a76c7fd690d14b97345626de73150be69fe7a0b9
Khiem Huynh <trongkhiem.huynh@gmail.com>
2020-09-06 22:19:47 +0700

    Initial Commit

---
+++ HTK Memories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HTK Memories
+//
+//  Created by khiemht on 9/6/20.
+//  Copyright © 2020 HTK Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
